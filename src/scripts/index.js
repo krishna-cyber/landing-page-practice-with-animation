eecbb6fabbda133f0b29407e4d5ae3fd67e75a5f
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-09-29 21:29:38 +0545

    js file added

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 50,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    }
+  });
